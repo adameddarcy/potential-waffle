d8e481ccffcf5b0273b317b556f922fa91fbcfdf
adam.darcy <adam.darcy@workday.com>
2022-11-10 16:26:27 +0000

    used java secureRandom to generate a cookie value

--- src/java/validate/ValidateLogin.java
+++ src/java/validate/ValidateLogin.java
@@ -24,7 +24,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.security.NoSuchAlgorithmException;
 
 /**
  *
